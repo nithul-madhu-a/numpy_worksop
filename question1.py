3e1651cb104b0c182df0c5bc90041c1e83df0bbf
NITHUL D MADHU <nithulmadhu.a2023ai-ds@sece.ac.in>
2024-05-20 14:16:19 +0530

    ADDED ANSWER

--- question1.py
+++ question1.py
@@ -1,1 +1,6 @@
-# write a python code to find if the given number is odd or even?
+a = int( input("Enter a Number: "))
+
+if ( a % 2 == 0 ):
+  print("Number is Even")
+else:
+  print("Number is Odd")# write a python code to find if the given number is odd or even?
